03ea21c6f716843267ca6261f17229a654298689
tobias <tobias.omondi@student.moringaschool.com>
2023-10-30 21:47:58 +0300

    upgraded

---
+++ code challeng/client/my-pizza/src/App.js
@@ -0,0 +1,36 @@
+// import { Switch, Route } from "react-router-dom";
+// import Header from "./Header";
+// import Restaurant from "./Restaurant"; 
+// import Home from "./Home";
+// import RestaurantPizza from "./RestaurantPizza"; 
+// import Pizza from "./Pizza"; 
+// import PizzaEditForm from "./PizzaEditForm"; 
+
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//       <main>
+//         <Switch>
+//           <Route exact path="/restaurant_pizzas/new">
+//             <RestaurantPizza />
+//           </Route>
+//           <Route exact path="/pizzas/:id/edit">
+//             <PizzaEditForm />
+//           </Route>
+//           <Route exact path="/pizzas/:id">
+//             <Pizza />
+//           </Route>
+//           <Route exact path="/restaurants/:id">
+//             <Restaurant />
+//           </Route>
+//           <Route exact path="/">
+//             <Home />
+//           </Route>
+//         </Switch>
+//       </main>
+//     </div>
+//   );
+// }
+
+// export default App;
